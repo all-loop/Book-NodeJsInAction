fa63a5425deeb6aad63828c129f3ed0afec7b42e
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-30 18:32:10 -0300

    part01-cap02 - Refactorización del ejemplo. Se reorganizo la lógica en funciones y se disminuyo el anidamiento de callbacks, ahora es más mantenible y legible

--- part-01/cap02 - Node programming Fundamentals/callbacks-example/blog_recent.js
+++ part-01/cap02 - Node programming Fundamentals/callbacks-example/blog_recent.js
@@ -6,31 +6,42 @@ const fs = require("fs");
 // solicitud entrante
 http
   .createServer((req, res) => {
-    if (req.url === "/") {
-      // Leemos el archivo JSON y usamos una callback para definir que hacer con su contenido
-      fs.readFile("./titles.json", (err, data) => {
-        if (err) {
-          console.error(err);
-          res.end("Server Error");
-        } else {
-          // Parseamos los datos del archivo JSON
-          const titles = JSON.parse(data.toString());
-          // Leemos la plantilla HTML y usamos un callback para cuando cargen los datos
-          fs.readFile("./template.html", (err, data) => {
-            if (err) {
-              console.error(err);
-              res.end("Server Error");
-            } else {
-              const tmpl = data.toString();
-              // Ensamblamos el contenido de la pág HTML para mostrar los títulos del blog
-              const html = tmpl.replace("%", titles.join("</li><li>"));
-              res.writeHead(200, { "Content-Type": "text/html" });
-              // Enviamos la página resultante al cliente
-              res.end(html);
-            }
-          });
-        }
-      });
-    }
+    getTitles(res);
   })
   .listen(8000, "127.0.0.1");
+
+function getTitles(res) {
+  // Leemos el archivo JSON y usamos una callback para definir que hacer con su contenido
+  fs.readFile("./titles.json", (err, data) => {
+    if (err) {
+      handError(err, res);
+    } else {
+      // Parseamos los datos del archivo JSON
+      getTemplate(JSON.parse(data.toString()), res);
+    }
+  });
+}
+
+function getTemplate(titles, res) {
+  // Leemos la plantilla HTML y usamos un callback para cuando cargen los datos
+  fs.readFile("./template.html", (err, data) => {
+    if (err) {
+      handError(err, res);
+    } else {
+      formatHtml(titles, data.toString(), res);
+    }
+  });
+}
+
+function formatHtml(titles, tmpl, res) {
+  // Ensamblamos el contenido de la pág HTML para mostrar los títulos del blog
+  const html = tmpl.replace("%", titles.join("</li><li>"));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  // Enviamos la página resultante al cliente
+  res.end(html);
+}
+
+function handError(err) {
+  console.log(err);
+  res.end("Server Error");
+}
